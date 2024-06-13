2af7039ab8e449eb33477b616cb87be600beabb4
hrusheekeshsawarkarreverie <148539120+hrusheekeshsawarkarreverie@users.noreply.github.com>
2024-06-13 16:50:37 +0530

    Add files via upload

--- llama-3.py
+++ llama-3.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from huggingface_hub import login
 
 from transformers import pipeline
 
 
+# Hugging Face token
+huggingface_token = "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"
 
-# pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B")
+# Login to Hugging Face (optional, primarily for the first setup)
+# login(token=huggingface_token)
+
+
+
+pipe = pipeline("text-generation", model="meta-llama/Meta-Llama-3-8B", token="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")
 # pipe = pipeline("text-generation", model="microsoft/Phi-3-mini-4k-instruct", trust_remote_code=True)
 pipe = pipeline("text-generation", model="Writer/palmyra-small")
 
